eca3c7ef1e574a8bc9170af7c1e706006db72b2e
loona-r <loona-r@users.noreply.github.com>
2018-02-27 14:54:59 +0100

    Model classes : person and city

--- Isen.DotNet.ConsoleApp/Program.cs
+++ Isen.DotNet.ConsoleApp/Program.cs
@@ -1,17 +1,34 @@
 ﻿using System;
 using Isen.DotNet.Library;
+using Isen.DotNet.Library.Models.Implementation;
 
 namespace Isen.DotNet.ConsoleApp
 {
     class Program
     {
+        /****** HELLO WORLD ******
         static void Main(string[] args)
         {
-            string result = Hello.Greet("Loona");
-            Console.WriteLine(result);
+            string world = Hello.World;
+            Console.WriteLine(world);
 
-            string result2 = Hello.World;
-            Console.WriteLine(result2);
+            string greet = Hello.Greet("Loona");
+            Console.WriteLine(greet);
+
+            string greetUpper = Hello.GreetUpper("Loona");
+            Console.WriteLine(greetUpper);
+        }*/
+
+        static void Main(string[] args)
+        {
+            var me = new Person
+            {
+                FirstName = "Loona",
+                LastName = "Rivière",
+                BirthDate = new DateTime(1997,1,17),
+                City = new City { Name = "Toulon" },
+            };
+            Console.WriteLine(me);
         }
     }
 }
